d2e5867fadb69918c77f8eac7d7e0f572c88db56
Oskar Hulter <stresslessness91@gmail.com>
2024-03-15 22:29:06 +0100

    conf up

--- astro.config.ts
+++ astro.config.ts
@@ -2,10 +2,27 @@ import db from '@astrojs/db';
 import node from '@astrojs/node';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
+import orama from '@orama/plugin-astro'
 
+
+// https://astro.build/config
 export default defineConfig({
 	integrations: [
 		db(),
+    orama({
+      // We can generate more than one DB, with different configurations
+      mydb: {
+        // Required. Only pages matching this path regex will be indexed
+        pathMatcher: /blog\/[0-9]{4}\/[0-9]{2}\/[0-9]{2}\/.+$/,
+
+        // Optional. 'english' by default
+        language: 'spanish',
+
+        // Optional. ['body'] by default. Use it to constraint what is used to
+        // index a page.
+        contentSelectors: ['h1', 'main'],
+      },
+    }),
 		tailwind({
 			nesting: true,
 		}),
@@ -14,4 +31,4 @@ export default defineConfig({
 	adapter: node({
 		mode: 'standalone',
 	}),
-});
+})
